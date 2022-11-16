2309f67ed9426d85a7c114831a9836740d522707
Raul Arias Perez <aprariasperez@gmail.com>
2022-11-16 11:29:31 +0100

    cambios menu

---
+++ src/js/menu.js
@@ -0,0 +1,8 @@
+var menu = document.getElementById('despmenu');
+
+document.getElementById('button').addEventListener('click', evt=>
+{
+    menu.style.display = 'block';
+    menu.style.textAlign = 'right';
+    menu.style.backgroundColor = '#0000007c';
+});
