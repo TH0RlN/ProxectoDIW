66cfb72a04a2c925aaf9dfb9a313c3b140040e3c
Raul Arias Perez <aprariasperez@gmail.com>
2022-11-16 13:28:13 +0100

    mas cambios nav

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,8 +1,17 @@
 var menu = document.getElementById('despmenu');
+var selected = -1;
 
 document.getElementById('button').addEventListener('click', evt=>
 {
-    menu.style.display = 'block';
-    menu.style.textAlign = 'right';
-    menu.style.backgroundColor = '#0000007c';
+    if (selected < 0)
+    {
+        menu.style.display = 'block';
+        menu.style.textAlign = 'right';
+        menu.style.backgroundColor = '#0000007c';
+    }
+    else
+    {
+        menu.style.display = 'none';
+    }
+    selected *= -1;
 });
